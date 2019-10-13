467cdb690d49e2e51db304468405a5e5d393d7d6
Jérémie Kalfon <jkobject@gmail.com>
2019-10-13 19:32:41 -0400

    new site version

--- _site/service-worker.js
+++ _site/service-worker.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"22d1b80384095d4f0e518a680b567ed1"},{"url":"/blog/umap-explanation/","revision":"7fb59c55b654cbe319edeebfc4a1ecc8"},{"url":"/blog/a-month-at-the-broad/","revision":"6d920c7dbfc49674c36831ce2928c718"},{"url":"/blog/interviews-at-epfl/","revision":"21dad48e3275677b5d05213a98403e73"},{"url":"/blog/broadinstitute-interview/","revision":"739ba1e7b2846d6aa60dcda1f5cfc66e"},{"url":"/blog/interviews-in-cambridge/","revision":"bb4f8f4d3813b0c5756a619ee9820e61"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"e385d9f161619fee661b3509843c9e5e"},{"url":"/blog/umap-explanation/","revision":"afde3b3c6cdca2a08f42d7ac3657bf19"},{"url":"/blog/a-month-at-the-broad/","revision":"6d920c7dbfc49674c36831ce2928c718"},{"url":"/blog/interviews-at-epfl/","revision":"21dad48e3275677b5d05213a98403e73"},{"url":"/blog/broadinstitute-interview/","revision":"739ba1e7b2846d6aa60dcda1f5cfc66e"},{"url":"/blog/interviews-in-cambridge/","revision":"bb4f8f4d3813b0c5756a619ee9820e61"}];
             // service-worker.js
 
 // set names for both precache & runtime cache
