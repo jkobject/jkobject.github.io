26140b561463a1d609b7cb165cf0d1136487505d
Jérémie Kalfon <jkobject@gmail.com>
2019-12-09 11:00:31 -0500

    new site version

--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1575906568').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1575907180').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
